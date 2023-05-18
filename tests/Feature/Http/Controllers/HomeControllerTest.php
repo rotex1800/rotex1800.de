ef3b0e7080d8f6d6d550aec9f0baf6065870aa08
Paul Schuberth <github@pschuberth.de>
2023-05-18 23:52:04 +0200

    POC rendering Markdown content

--- tests/Feature/Http/Controllers/HomeControllerTest.php
+++ tests/Feature/Http/Controllers/HomeControllerTest.php
@@ -8,3 +8,10 @@ test('has /home route', function () {
 test('/home route is named "home"', function () {
     expect(route('home'))->toEndWith('/home');
 });
+
+it('contains "Rotex 1800" in headline', function () {
+    $this->get(route('home'))
+        ->assertElementExists('h1', function (\Sinnbeck\DomAssertions\Asserts\AssertElement $element) {
+            $element->containsText('Rotex 1800');
+        });
+});
