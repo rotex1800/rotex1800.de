f591bd1b0efe7d68c8ca0200a8bdeef2bee46f50
Paul Schuberth <github@pschuberth.de>
2023-05-21 21:31:09 +0200

    Create Post from Markdown file with Hugo frontmatter

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,6 @@ use Spatie\LaravelMarkdown\MarkdownRenderer;
 
 class HomeController extends Controller
 {
-
     private MarkdownRenderer $markdown;
 
     public function __construct()
@@ -22,8 +21,8 @@ class HomeController extends Controller
     public function __invoke(Request $request)
     {
         $file = Storage::disk('content')->get('example.md');
-        return view('home')->with(['content' =>
-            $this->markdown->toHtml($file)
+
+        return view('home')->with(['content' => $this->markdown->toHtml($file),
         ]);
     }
 }
--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -47,11 +47,10 @@ return [
         'content' => [
             'driver' => 'local',
             'root' => storage_path('content'),
-            'url' => env('APP_URL') . '/content',
+            'url' => env('APP_URL').'/content',
             'visibility' => 'public',
             'throw' => false,
-        ]
-
+        ],
 
     ],
 
