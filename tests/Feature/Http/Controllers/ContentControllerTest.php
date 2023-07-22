aaa3ee973eba685819c99d9f296ca3f148aedbf8
Paul Schuberth <github@pschuberth.de>
2023-07-22 22:45:04 +0200

    chore: Add tailwind and tailwind-typography

--- tests/Feature/Http/Controllers/ContentControllerTest.php
+++ tests/Feature/Http/Controllers/ContentControllerTest.php
@@ -3,6 +3,12 @@
 use Sinnbeck\DomAssertions\Asserts\AssertElement;
 use Tests\TestData\FileContents;
 
+beforeEach(function () {
+    Storage::fake('content');
+    Storage::disk('content')->put('example.md', FileContents::EXAMPLE);
+    Artisan::call('content:refresh');
+});
+
 test('route named "home" redirects to "/home"', function () {
     $this->get(route('home'))
         ->assertRedirect('/home');
@@ -13,13 +19,18 @@ test('Storage facade')
     ->not->toBeUsedIn('App\Http\Controllers\ContentController');
 
 it('serves markdown file matching the request path', function () {
-    Storage::fake('content');
-    Storage::disk('content')->put('example.md', FileContents::EXAMPLE);
-    Artisan::call('content:refresh');
     $this->get('example')
         ->assertStatus(200)
         ->assertElementExists('h1', function (AssertElement $element) {
             $element->containsText('Rotex 1800');
             $element->doesntContainText('title');
+        });
+});
+
+it('uses tailwind typography', function () {
+    $this->get('example')
+        ->assertStatus(200)
+        ->assertElementExists('body', function (AssertElement $body) {
+            $body->has('class', 'prose');
         });
 });
