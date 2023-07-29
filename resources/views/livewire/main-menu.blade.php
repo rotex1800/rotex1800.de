2201261b624a0eb8f295ba06ffbddfb0471a5b3e
Paul Schuberth <github@pschuberth.de>
2023-07-29 22:13:38 +0200

    chore: Arrange menu items depending on space

--- resources/views/livewire/main-menu.blade.php
+++ resources/views/livewire/main-menu.blade.php
@@ -1,6 +1,6 @@
 <div>
     <nav class="bg-blue-500">
-        <ul>
+        <ul class="flex flex-col md:flex-row">
             @foreach($entries as $entry)
                 <li class="text-white text-lg block w-fit p-2 hover:underline transition-all duration-200 ease-in-out"><a href={{ url($entry->path) }}>{{ $entry->text }}</a></li>
             @endforeach
