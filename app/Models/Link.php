041d886129b2a74fd29fc4aef160f4d90dc4af6b
Paul Schuberth <github@pschuberth.de>
2023-07-30 17:20:44 +0200

    generate ide helper code

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -6,6 +6,28 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * App\Models\Link
+ *
+ * @property int $id
+ * @property string $path
+ * @property int $post_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Post|null $post
+ *
+ * @method static \Database\Factories\LinkFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Link newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Link newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Link query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link wherePath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link wherePostId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereUpdatedAt($value)
+ *
+ * @mixin \Eloquent
+ */
 class Link extends Model
 {
     use HasFactory;
